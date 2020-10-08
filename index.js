2f76ddc18a06a56e6b044cf73850427de7dcb1f3
play.js <git@gituser.com>
2020-10-07 22:49:23 -0600

    checking fetch on pc

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express();
 require('dotenv').config();
 
 app.get('/', async (req, res)=>{
-  //let twitterRes = fetch()
-  res.send(process.env.BEARER_TOKEN);
+  let NBAhashtag = encodeURIComponent('#nba')
+  let twitterRes = fetch(`https://api.twitter.com/1.1/search/tweets.json?q=`+NBAhashtag, {
+    headers: `authorization: Bearer ${process.env.BEARER_TOKEN}`
+ 
+  res.send("hello");
 });
 app.listen(3001, ()=>{
   console.log('listening on 3001')
