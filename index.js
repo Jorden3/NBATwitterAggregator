05ea610d010a65e56c10d003a9f8765585ce115e
play.js <git@gituser.com>
2020-10-08 21:00:07 -0600

    checking in pc

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const app = express();
 require('dotenv').config();
 
 app.get('/', async (req, res)=>{
-  let NBAhashtag = encodeURIComponent('#nba')
-  let twitterRes = fetch(`https://api.twitter.com/1.1/search/tweets.json?q=`+NBAhashtag, {
-    headers: `authorization: Bearer ${process.env.BEARER_TOKEN}`
+  let NBAhashtag = encodeURIComponent('#nba');
+  console.log(`https://api.twitter.com/1.1/search/tweets.json?q=${NBAhashtag}`)
+  try{
+  let twitterRes = await fetch(`https://api.twitter.com/1.1/search/tweets.json?q=${NBAhashtag}`, {
+    headers: {`authorization`: `Bearer ${process.env.BEARER_TOKEN}`}
+    })
+    }
+    catch(e){
+      console.log(e);
+    }
+    
  
   res.send("hello");
 });
