822d62aad5fcefdd49779db3142be17d0abdf3d8
Jorden Garcia <Jay@Tayllers-MacBook-Pro.local>
2020-10-08 21:25:25 -0600

    Calls twitter and has jshint es6

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 const app = express();
 require('dotenv').config();
 
 app.get('/', async (req, res)=>{
   let NBAhashtag = encodeURIComponent('#nba');
-  console.log(`https://api.twitter.com/1.1/search/tweets.json?q=${NBAhashtag}`)
+  let results = encodeURIComponent('popular');
+  console.log(`https://api.twitter.com/1.1/search/tweets.json?q=${NBAhashtag}&result_type=${results}`);
   try{
-  let twitterRes = await fetch(`https://api.twitter.com/1.1/search/tweets.json?q=${NBAhashtag}`, {
-    headers: {`authorization`: `Bearer ${process.env.BEARER_TOKEN}`}
-    })
+    let twitterRes = await fetch(`https://api.twitter.com/1.1/search/tweets.json?q=${NBAhashtag}`, {
+      headers: {authorization : `Bearer ${process.env.BEARER_TOKEN}`}
+    });
+    let twitterJson = await twitterRes.json();
+    res.send(twitterJson);
     }
     catch(e){
       console.log(e);
     }
     
  
-  res.send("hello");
+  
 });
 app.listen(3001, ()=>{
-  console.log('listening on 3001')
+  console.log('listening on 3001');
 });
